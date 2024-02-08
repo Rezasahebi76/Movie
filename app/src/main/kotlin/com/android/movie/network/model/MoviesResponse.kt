337f9ec1dabf4b0e7567c7a164ea69c38110f56e
Reza Sahebi <mohammadreza.sahebi@partdp.ir>
2024-02-08 23:01:04 +0330

    # fix MoviesResponse property name

--- app/src/main/kotlin/com/android/movie/network/model/MoviesResponse.kt
+++ app/src/main/kotlin/com/android/movie/network/model/MoviesResponse.kt
@@ -1,11 +1,14 @@
 package com.android.movie.network.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class MoviesResponse(
     val page: Int,
     val results: List<MovieResponse>,
+    @SerialName("total_pages")
     val totalPages: Int,
+    @SerialName("total_results")
     val totalResults: Int
 )
