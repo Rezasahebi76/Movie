f59512511861ae08b985597e5220e15b1aa1a531
Reza Sahebi <mohammadreza.sahebi@partdp.ir>
2024-02-15 00:38:13 +0330

    # improve Typography

--- app/src/main/kotlin/com/android/movie/ui/theme/Type.kt
+++ app/src/main/kotlin/com/android/movie/ui/theme/Type.kt
@@ -2,33 +2,58 @@ package com.android.movie.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.android.movie.R
 
-// Set of Material typography styles to start with
+private val MovieFontFamily: FontFamily = FontFamily(
+    Font(R.font.inter_regular, FontWeight.Normal),
+    Font(R.font.inter_medium, FontWeight.Medium),
+    Font(R.font.inter_semi_bold, FontWeight.SemiBold),
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+    titleLarge = TextStyle(
+        fontFamily = MovieFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 24.sp,
+    ),
+    titleMedium = TextStyle(
+        fontFamily = MovieFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        lineHeight = 20.sp,
+    ),
+    titleSmall = TextStyle(
+        fontFamily = MovieFontFamily,
+        fontWeight = FontWeight.Medium,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    labelLarge = TextStyle(
+        fontFamily = MovieFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+    ),
+    labelMedium = TextStyle(
+        fontFamily = MovieFontFamily,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        fontSize = 14.sp,
+        lineHeight = 24.sp,
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = MovieFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 24.sp,
+    ),
+
+    bodyMedium = TextStyle(
+        fontFamily = MovieFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 12.sp,
+    ),
 )
